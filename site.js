1fcae7bdce6d72552da4288fcd89d3d93da8f9b4
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2021-03-05 12:13:49 +0530

    Add files via upload

---
+++ site.js
@@ -0,0 +1,20 @@
+if (getParameterByName('owner')) {
+    var owner = getParameterByName('owner');
+    dj('header h1').html('Glow - Inside ~ ' + owner)
+}
+var yourname = "Your Name";
+let json = get.json('https://raw.githubusercontent.com/glowinside/json/main/1.json')
+var content = "";
+for (i = 0; i < json.length; i++) {
+    var rand = Math.floor((Math.random() * parseInt(json.length)));
+    csl(rand);
+    content += `<div class="image">
+    <span class="title">${json[rand].title +" ~ "+ owner}</span><br>
+    <img src="${json[rand].url}" class="post-img">
+    <a href="` + json[rand].url + `" download="Glow - Inside.jpg">
+    <img class="download" src="download.png"></a>
+    <a href="whatsapp://send?text=${location.origin + location.pathname + "?owner="+yourname}"  data-action="share/whatsapp/share"  
+    target="_blank"> <img class="whatsapp" src="whatsapp.png"></a></div>`;
+}
+
+dj('.gocontainer').html(content);
